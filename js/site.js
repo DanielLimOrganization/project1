c09f3df7fec7786739f1389c2fb8c31cfb0b0900
Daniel Lim <dlim7@hawk.iit.edu>
2023-03-02 20:51:21 -0600

    attempting js implementation

--- js/site.js
+++ js/site.js
@@ -3,3 +3,20 @@ jQuery(function($) {
   $('html').removeClass('nojs');
   $('html').addClass('hasjs');
 });
+function init(){
+  
+  var Name = document.getElementById('Name')
+  var pressButton = document.getElementById('submit')
+  var output = document.getElementById('textoutput')
+  
+  function alertFunc() {
+    //sends the alert
+    alert('Daniel Lim: ' + Name.value)
+    //changes h2
+    output.innerHTML = Name.value + ' account created';
+  }
+  
+  pressButton.addEventListener('click', alertFunc);
+
+}
+window.addEventListener('load', init);
